6d7c192cd64ac294f4bd52d369ef382118f79019
Alice <kewinw2508@gmail.com>
2026-09-16 19:06:26 -0300

    Calculadora v4

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -4,6 +4,15 @@ function Calculadora() {
     const [valor1, setValor1] = useState(0);
     const [valor2, setValor2] = useState(0);
 
+    function calcular () {
+        const numero1 = Number(valor1);
+        const numero2 = Number(valor2);
+        const soma = numero1 + numero2;
+        const subtracao = numero1 - numero2;
+        const multiplicacao = numero1 * numero2;
+        const divisao = numero1 / numero2;
+    }
+
   return (
     <div>
       <h1>Caixa de Mercado</h1>
@@ -11,7 +20,7 @@ function Calculadora() {
       <input type="number" value={valor1} onChange={(evento) => setValor1(evento(e.target.value))} />
       <input type="number" value={valor2} onChange={(evento) => setValor2(evento(e.target.value))} />
 
-      <button>Calcular</button>
+      <button onClick={calcular}>Calcular</button>
 
     </div>
   );
