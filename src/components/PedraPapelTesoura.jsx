4401881fcf6ed3119bca74afa2113f59e9ec379c
Alice <kewinw2508@gmail.com>
2026-09-16 20:58:40 -0300

    Finalização exercicio 3

--- src/components/PedraPapelTesoura.jsx
+++ src/components/PedraPapelTesoura.jsx
@@ -5,6 +5,8 @@ function PedraPapelTesoura() {
     const [jogadaJogador, setJogadaJogador] = useState('');
     const [jogadaComputadorAtual, setJogadaComputadorAtual] = useState('');
     const [resultado, setResultado] = useState('');
+    const [placarJogador, setPlacarJogador] = useState(0);
+    const [placarComputador, setPlacarComputador] = useState(0);
 
 
     function jogadaComputador() {
@@ -15,7 +17,7 @@ function PedraPapelTesoura() {
     function jogar(jogador) {
         const computador = jogadaComputador();
         setJogadaComputadorAtual(computador);
-        verificarVencedor(jogadaJogador, computador);
+        verificarVencedor(jogador, computador);
     }
 
     function verificarVencedor(jogador, computador) {
@@ -25,20 +27,22 @@ function PedraPapelTesoura() {
                    (jogador === 'Papel' && computador === 'Pedra') ||
                    (jogador === 'Tesoura' && computador === 'Papel')){
             setResultado('Você venceu!');
+            setPlacarJogador((valorAnterior) => valorAnterior + 1);
         } else {
             setResultado('Computador venceu!');
+            setPlacarComputador((valorAnterior) => valorAnterior + 1);
         }
                    }
     return (
         <div>
             <h1>Pedra, Papel ou Tesoura</h1>
-            <button onClick={jogadaComputador}>Jogar</button>
             <button onClick={() => { setJogadaJogador('Pedra'); jogar('Pedra'); }}>Pedra</button>
             <button onClick={() => { setJogadaJogador('Papel'); jogar('Papel'); }}>Papel</button>
             <button onClick={() => { setJogadaJogador('Tesoura'); jogar('Tesoura'); }}>Tesoura</button>
             <p>Sua jogada: {jogadaJogador}</p>
             <p>Jogada do computador: {jogadaComputadorAtual}</p>
             <p>Resultado: {resultado}</p>
+            <p>Placar - Você: {placarJogador} x Computador: {placarComputador}</p>
         </div>
     );
 }
