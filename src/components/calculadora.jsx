8636b93859ad7b61ad9f005454d85bf75a316702
Alice <kewinw2508@gmail.com>
2026-09-16 18:57:30 -0300

    CalculadoraV2

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -2,6 +2,12 @@ function Calculadora() {
   return (
     <div>
       <h1>Caixa de Mercado</h1>
+
+      <input type="number" />
+      <input type="number" />
+
+      <button>Calcular</button>
+      
     </div>
   );
 }
