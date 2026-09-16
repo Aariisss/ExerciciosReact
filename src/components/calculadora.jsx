420cf40125c4384d3ba6ffafca12d9041d5d8b9e
Alice <kewinw2508@gmail.com>
2026-09-16 19:02:57 -0300

    Calculadora v3

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
+
 function Calculadora() {
+    const [valor1, setValor1] = useState(0);
+    const [valor2, setValor2] = useState(0);
+
   return (
     <div>
       <h1>Caixa de Mercado</h1>
 
-      <input type="number" />
-      <input type="number" />
+      <input type="number" value={valor1} onChange={(evento) => setValor1(evento(e.target.value))} />
+      <input type="number" value={valor2} onChange={(evento) => setValor2(evento(e.target.value))} />
 
       <button>Calcular</button>
-      
+
     </div>
   );
 }
