637a49165253ffe6b8604c7777b96ec163e3eb6d
Alice <kewinw2508@gmail.com>
2026-09-16 19:51:30 -0300

    Adivinhação v2

--- src/components/adivinhacao.jsx
+++ src/components/adivinhacao.jsx
@@ -1,7 +1,25 @@
+import useState from 'react';
+
 function Adivinhação() {
+    const [numeroSecreto, setNumeroSecreto] = useState(Math.floor(Math.random() * 100) + 1);
+    const [tentativa, setTentativa] = useState('');
+
+    function adivinharTentativa() {
+        const numeroTentativa = Number(tentativa);
+        if (numeroTentativa > numeroSecreto) {
+            console.log('Muito alto');
+        } else if (numeroTentativa < numeroSecreto) {
+            console.log('Muito baixo');
+        } else {
+            console.log('Acertou!');
+        }
+    }
+
     return (
         <div>
             <h1>Jogo de Adivinhação</h1>
+            <input type="number" value={tentativa} onChange={(evento) => setTentativa(evento.target.value)} />
+            <button onClick={adivinharTentativa}>Tentar</button>
         </div>
     );
 }
